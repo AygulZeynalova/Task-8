a9f206b909a5f3757e335f829d2e19d1ebde6079
Aygul Zeynalova <aygl.zeynalova@mail.ru>
2024-02-02 16:08:15 +0400

    Task 6

---
+++ Task8Students/Task8Students/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Data;
+
+namespace Task8Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[0];
+            int studentCount = 0;
+            do
+            {
+                Console.WriteLine("Enter the student information:");
+
+                Console.Write("Name:");
+                string name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                    break;
+
+                Console.Write("Surname:");
+                string surname = Console.ReadLine();
+
+                Console.Write("Date of birth (MM/dd/yyyy): ");
+                string input = Console.ReadLine();
+
+                DateTime dateOfBirth;
+                if (DateTime.TryParse(input, out dateOfBirth))
+                {
+                    Array.Resize(ref students, studentCount + 1);
+                    students[studentCount] = new Student { Name = name, Surname = surname, DateOfBirth = dateOfBirth };
+                    
+                    studentCount++;
+                }
+                else
+                {
+                    Console.WriteLine("You have entered an incorrect date.");
+                }
+
+                foreach (var student in students)
+                {
+                    if (student != null)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+
+                Console.WriteLine("Press Enter to continue");
+
+            } while (Console.ReadKey().Key == ConsoleKey.Enter);
+
+            Console.WriteLine("\nAll the students that you inputed: ");
+
+            foreach (var student in students)
+            {
+                if (student != null)
+                {
+                    Console.WriteLine(student);
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Task8Students/Task8Students/StudentInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8Students
+{
+    internal class Student
+    {
+        static int counter = 0;
+        public Student()
+        {
+            this.Id = ++counter;
+        }
+        public int Id { get;private set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public override string ToString()
+        {
+            Console.WriteLine("=====STUDENT INFO=====");
+            return $"ID:{Id},Name:{Name},Surname:{Surname},Date of birth:{DateOfBirth}";
+        }
+
+    }
+}
